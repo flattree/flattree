956c3554640ebfb93ca800efc8b48d9a15d65d4b
Aleksandr Mikhailov <dev@avidclam.com>
2020-01-29 13:47:42 +0300

    logic with unflatten bug

--- flattree/logic.py
+++ flattree/logic.py
@@ -1,8 +1,9 @@
 from collections.abc import Mapping, MutableSequence
-from collections import ChainMap
+from collections import ChainMap, deque
+import re
 
-#SELR = ('.', '\\', '\u23A1', '\u23A6')
-SELR = ('.', '\\', '[', ']')
+_BS = '\\'
+SELR = ('.', _BS, '\u23A1', '\u23A6')
 
 
 def genleaves(*trees, prepend=None, selr=SELR):
@@ -22,8 +23,8 @@ def genleaves(*trees, prepend=None, selr=SELR):
     if prepend is None:
         prepend = []
         prefix = None
-    else:
-        prefix = sep.join([x.replace(sep, esc + sep) for x in prepend])
+    else:  # escape separator
+        prefix = sep.join(x.replace(sep, esc + sep) for x in prepend)
     lead_tree = trees[0]
     if isinstance(lead_tree, MutableSequence):  # a list
         for i, el in enumerate(lead_tree):
@@ -62,16 +63,14 @@ def flatkey_to_keylist(flatkey, selr=SELR):
     return [x.replace('\r', sep) for x in keylist] if esc and sep else keylist
 
 
-def unflatten(flatdata, branch, selr=SELR, default=None, raise_key_error=False):
-    """Restores nested dictionaries from a flat tree starting with root.
-
-    Calling ``unflatten(flatten(x))`` should return ``x``
-    (keys may be sorted differently if ``x`` is a dictionary).
+def unflatten(flatdata, branch=None, selr=SELR,
+              default=None, raise_key_error=False):
+    """Restores nested dictionaries from a flat tree starting with a branch.
 
     Args:
-        flatdata: dictionary of values indexed by "flat" keys
+        flatdata (dict): dictionary of values indexed by flatkeys
         branch: substring of branch to restore (None for the whole tree)
-        selr: tuple of Separator, Escape, Left Bracket, Right Bracket symbols
+        selr (tuple): Separator, Escape, Left Bracket, Right Bracket symbols
         default: default value
             Returned in case no branch is found and raise_key_error is False.
         raise_key_error (bool): if True, raise exception rather than
@@ -86,7 +85,7 @@ def unflatten(flatdata, branch, selr=SELR, default=None, raise_key_error=False):
         return flatdata[branch]
     # flabra: flat branch, a flat subtree of a flat tree, let's build it
     sep, esc, lb, rb = selr
-    build_list = False
+    build_list = False  # lists are legit part of trees (since v2)
     if branch is None:
         flabra = flatdata  # the whole tree
         if any(flatkey.startswith(lb) for flatkey in flabra):
@@ -99,16 +98,15 @@ def unflatten(flatdata, branch, selr=SELR, default=None, raise_key_error=False):
             elif flatkey.startswith(branch + lb):
                 build_list = True
                 flabra[flatkey[len(branch):]] = value
-    # Check if flat branch has anything in it
-    # if not: return default of raise error
+    # Check if flat branch is not empty, otherwise return default of raise error
     if not flabra:
         if raise_key_error:
             raise KeyError(branch)
         else:
             return default
-    # Now build a tree or a list
+    # Now build a tree
     if build_list:
-        # first, let's build a "sparse list"
+        # first, let's build an intermediate "sparse list"
         sparse = {}
         for flatkey, value in flabra.items():
             if flatkey.startswith(lb):
@@ -117,13 +115,13 @@ def unflatten(flatdata, branch, selr=SELR, default=None, raise_key_error=False):
                     sparse[int(idxkey[1:-1])] = (downkeys, value)
                 except (IndexError, ValueError):
                     pass
-        if not sparse:  # everything was bad quality
+        if not sparse:  # all keys were discarded
             if raise_key_error:
                 raise KeyError(branch)
             else:
                 return default
-        else:  # let's get real list from the sparse one
-            result = [None] * (max(sparse.keys()) + 1)
+        else:  # build real list from the sparse one
+            result = [None] * (max(sparse.keys()) + 1)  # placeholder
             for i in sparse:
                 downkeys, value = sparse[i]
                 if downkeys:
@@ -133,12 +131,17 @@ def unflatten(flatdata, branch, selr=SELR, default=None, raise_key_error=False):
                                           raise_key_error=False)
                 else:
                     result[i] = value
-    else:  # need to build dictionary, not a list
+    else:  # build dictionary, not a list
         result = {}
+        list_ptn = ("(.+[^" + _BS + esc + "])" +
+                    _BS + lb + r"\d+" + _BS + rb + "$")
+        re_list_ptn = re.compile(list_ptn)
         for flatkey, value in flabra.items():
             sub_branch = flatkey_to_keylist(flatkey, selr=selr)[0]
-            if lb in sub_branch and rb in sub_branch:
-                sub_branch = sub_branch.split(lb)[0]
+            # If sub-branch is a list element, extract list name
+            ptn_match = re.match(re_list_ptn, sub_branch)
+            if ptn_match:
+                sub_branch = ptn_match.group(1)
             result[sub_branch] = unflatten(flabra, sub_branch, selr=SELR,
                                            default=default,
                                            raise_key_error=False)
