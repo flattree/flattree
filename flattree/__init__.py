01e2e225424d2e7b48bee8eaeec8294632b94113
Aleksandr Mikhailov <dev@avidclam.com>
2020-02-07 14:54:26 +0300

    v2.0.1

--- flattree/__init__.py
+++ flattree/__init__.py
@@ -4,7 +4,7 @@ FlatTree is a tool to work with nested Python dictionaries.
 
 __title__ = __name__
 __description__ = __doc__.replace('\n', ' ').replace('\r', '').strip()
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Aleksandr Mikhailov'
 __author_email__ = 'dev@avidclam.com'
 __copyright__ = '2020 Aleksandr Mikhailov'
